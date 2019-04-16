f6d324912e804ca5d47588ddc750a2339be781ff
Simantini Huchche <simantini@Drs-MacBook-Air.local>
2019-04-16 21:21:20 +0530

    code for models and partial code for handling excel uploads

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Medicines, Centres, Suppliers
+
+admin.site.register(Medicines)
+admin.site.register(Centres)
+admin.site.register(Suppliers)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Medicines(models.Model):
+
+    med_name = models.CharField(max_length=100)
+    mean_demand = models.FloatField(default=0)
+    std_dev = models.FloatField(default=0)
+    Holding_cost = models.FloatField(default=0)
+    Price = models.FloatField(default=0)
+    Ordering_cost = models.FloatField(default=0)
+
+    def __str__(self):
+        return f'{self.med_name}'
+
+class Centres(models.Model):
+    Centre_name = models.CharField(max_length=100)
+    med_name = models.CharField(max_length=100)
+    Lead_time = models.FloatField(default=0)
+    Std_dev = models.FloatField(default=0)
+
+    def __str__(self):
+        return f'{self.Centre_name}'
+
+class Suppliers(models.Model):
+    Supp_name = models.CharField(max_length=100)
+    med_name = models.CharField(max_length=100)
+    LT = models.FloatField(default=0)
+    Sdev = models.FloatField(default=0)
+
+    def __str__(self):
+        return f'{self.Supp_name}'
